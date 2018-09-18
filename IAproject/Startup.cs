61fc977563c17d890b41b20d35ec9e46dd962670
woodfor <woodforlol@gmail.com>
2018-09-18 19:40:35 +1000

    Fisrt vision

---
+++ IAproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IAproject.Startup))]
+namespace IAproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
